33e354091513174f4c3ba2fba7698c8e33302813
Sergio B <sbarajas510@gmail.com>
2019-05-10 01:01:36 -0700

    blog project started

---
+++ jquerybasics_coursefiles/jquery_v_js_example/js/app.js
@@ -0,0 +1,21 @@
+/*---Hide The Box---*/
+
+//using javascript
+// const box = document.querySelector('.box');
+// box.style.display= 'none';
+
+/*----Click event---*/
+// box.addEventListener('click',() => {
+//     alert('You clicked me!')
+// })
+
+//using jQuery
+
+//jQuery('.box').hide();
+// $('.box').hide();
+// $('.box').show();
+
+/*----Click event---*/
+// $('.box').click(function(){
+//     alert('You click me with jQuery');
+// })
