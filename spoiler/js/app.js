b42f8ae7fba71f77066fa777f80fb037fa7bf9cf
Sergio B <sbarajas510@gmail.com>
2019-05-11 23:26:05 -0700

    finished the spoiler project

--- spoiler/js/app.js
+++ spoiler/js/app.js
@@ -1,19 +1,65 @@
-// When the button is pressed 
-$('.spoiler').on('click', 'button', function(){
-  //Show the spoiler text
-  $(this).prev().show();
-  //Hide the "Reveal Spoiler" button
-  $(this).hide();
-});
+// // When the button is pressed 
+// $('.spoiler').on('click', 'button', function(){
+//   //Show the spoiler text
+//   $(this).prev().show();
+//   //Hide the "Reveal Spoiler" button
+//   $(this).hide();
+// });
 
-// Create the "Reveal Spoiler" button
-const $button = $('<button>Reveal Spoiler</button>');
+// // Create the "Reveal Spoiler" button
+// const $button = $('<button>Reveal Spoiler</button>');
+// //Append to web page
+// $('.spoiler').append($button);
+
+// //Hide the spoiler text
+// $('.spoiler span').hide();
+//<------------my code----------->//
+
+//// assing 1 event handler to multiple events 
+// $('#elemetn').on('click keypress', function(){
+// do stuff when the element is clicked or when a key is pressed
+// });
+
+//Create the "Reveal Spoiler" button 
+
+const $button = $("<button>Reveal Spoiler</button>");
 //Append to web page
-$('.spoiler').append($button);
+$(".spoiler").append($button);
 
-//Hide the spoiler text
-$('.spoiler span').hide();
+// //event delegation 
+// $(".spoiler button").on('click',function(){
+  
+//   //show the spoiler text
+//   $(".spoiler span").show();
+//   // hide the "Reveal Spoiler" button
+//   $(".spoiler button").hide();
+// })
+$('.spoiler').on('click','button',function(e){
+  console.log(e.target); 
+  //show the spoiler text
+  ///----You can also use the keyword "this"----///
+  //$(e.target).prev().show();
+  $(this).prev().show();
+  // hide the "Reveal Spoiler" button
+  //$(e.target).hide();
+  $(this).hide();
+})
 
 
+//hide the spoiler text 
+$(".spoiler span").hide();
+//when button is pressed 
+// $(".spoiler button").on('click',function(){
+  
+//   //show the spoiler text
+//   $(".spoiler span").show();
+//   // hide the "Reveal Spoiler" button
+//   $(".spoiler button").hide();
+// })
 
+// //------test of my own---///
+// $('.spoiler').on('mouseleave',function(){
+//   $('spoiler span').hide();
+//   $('spoiler button').show();
 
+// });
