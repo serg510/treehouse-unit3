f2f48053b8fa26112029de8659f2ff081a790435
Sergio B <sbarajas510@gmail.com>
2019-05-11 17:17:45 -0700

    deleted files

---
+++ blog_project/js/app.js
@@ -0,0 +1,45 @@
+// $('#flashMessage').hide()
+
+// $('#previewButton').click(function() {
+//   const title = $("#blogTitleInput").val();
+//   const content = $("#blogContentInput").val();
+
+//   $("#blogTitlePreview").text(title);
+//   $("#blogContentPreview").html(content);
+
+//   $("#flashMessage")
+//     .slideDown(1000)
+//     .delay(3000)
+//     .slideUp();
+// });
+
+// $('#flashMessage')
+//     .hide()
+//     .slideDown(1000)
+//     .delay(3000)
+//     .slideUp();
+
+
+// const title = 'My first Blog Post';
+
+// //.text wont work on html ex: <strong> use .html
+// const content = 'This is my <strong>first</strong> post';
+
+// $('#blogTitlePreview').text(title);
+// //$('#blogContentPreview').text(content);
+// $('#blogContentPreview').html(content);
+$('#flashMessage').hide();
+
+$('#previewButton').click(function(){
+    const title = $('#blogTitleInput').val();
+    const content = $('#blogContentInput').val();
+
+ $('#blogTitlePreview').text(title);
+
+$('#blogContentPreview').html(content);
+$('#flashMessage')
+    .slideDown(1000)
+    .delay(3000)
+    .slideUp();
+
+});
